a978a66a0f1fa9866ba4a33f373db47870a771a8
ijnam7 <ijnam7@gmail.com>
2023-05-08 19:12:07 +0900

    added memo and button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [메모내용, 메모내용변경] = useState(["메모내용1", "메모내용2"]);
+
   return (
     <div className="App">
       <div className="nav">
         <div> NEMO </div>
       </div>
       <h4 style={ {color: "gray"} }> NEMO는 채팅형 메모 사이트입니다. </h4>
+      <div className="list">
+        <hr/>
+        <div className="memo">{ 메모내용[0] }</div>
+        <p style={ {textAlign: "right"} }>2023년 5월 8일</p>
+        <button onClick={ ()=>{} }>입력</button>
+      </div>
     </div>
   );
 }
